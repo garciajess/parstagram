87bb3eb8e93223f45519d4b7370abc233a91fa4e
jessgarcia <60108571+cia14@users.noreply.github.com>
2020-10-23 21:54:05 -0700

    launches camera and saves data to Parse

---
+++ parstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  parstagram
+//
+//  Created by Jessica Garcia on 10/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
