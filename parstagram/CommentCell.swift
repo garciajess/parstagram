7be6fa7b868200d00608f8ebb17b3ad205c75a20
jessgarcia <60108571+cia14@users.noreply.github.com>
2020-10-31 01:19:08 -0700

    update allows user to add new comments and displays them

---
+++ parstagram/CommentCell.swift
@@ -0,0 +1,30 @@
+//
+//  CommentCell.swift
+//  parstagram
+//
+//  Created by Jessica Garcia on 10/30/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
